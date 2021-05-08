b0a85eb9068a1ca500d372c6a145226cb83bb207
Halil İbrahim ARAÇ <iboarac@gmail.com>
2021-05-08 11:22:15 +0300

    Category>Create tamamlandı. EditCategoryVM üzerinde validation için gerekli attribute'ler uygulandı.Client Side Validation devreye alındı.

--- IpekStore.Web/Controllers/CategoryController.cs
+++ IpekStore.Web/Controllers/CategoryController.cs
@@ -1,6 +1,6 @@
 ﻿using IpekStore.Web.Context;
 using IpekStore.Web.Models.Entities;
-using IpekStore.Web.Models.VM;
+using IpekStore.Web.Models.VM.Category;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -28,6 +28,34 @@ namespace IpekStore.Web.Controllers
                     Status = c.IsActive
                 });
             return View(categories);
+        }
+
+        [HttpGet]
+        //[AcceptVerbs("GET")]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        //[AcceptVerbs("POST")]
+        public IActionResult Create(EditCategoryVM categoryVM)
+        {
+            if (!ModelState.IsValid)
+                return View(categoryVM);
+
+            _context.Categories.Add(new Category
+            {
+                Name=categoryVM.Name,
+                IsActive=categoryVM.IsActive,
+                CreateUser="admin",
+                LastupUser="admin",
+                CreateDate=DateTime.Now,
+                LastupDate=DateTime.Now
+            });
+            _context.SaveChanges();
+
+            return RedirectToAction("List");
         }
 
         public IActionResult Edit(int id)
---
+++ IpekStore.Web/Models/VM/Category/EditCategoryVM.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IpekStore.Web.Models.VM.Category
+{
+    public class EditCategoryVM
+    {
+        [ScaffoldColumn(false)]
+        public int Id { get; set; }
+
+        [Display(Name="Kategori adı")]
+        [Required(ErrorMessage ="Kategori adı girilmelidir.")]
+        [MinLength(3,ErrorMessage ="Kategori adı en az 3 karakter olmalıdır.")]
+        [MaxLength(30,ErrorMessage ="Kategori adı en fazla 30 karakter olabilir.")]
+        public string Name { get; set; }
+
+        [Display(Name="Aktif kategori")]
+        public bool IsActive { get; set; }
+    }
+}

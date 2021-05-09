56068e71f57c3736b55865483f1405fdb326d492
Halil İbrahim ARAÇ <iboarac@gmail.com>
2021-05-09 12:42:18 +0300

    JConfirm kütüphanesi yapılandırıldı. Category>List içerisinde kullanılmak üzere site.js dosyasına gerekli fonksiyonlar eklendi.

--- IpekStore.Web/wwwroot/js/site.js
+++ IpekStore.Web/wwwroot/js/site.js
@@ -1,4 +1,68 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+var STATUS = {
+    Ok: 0,
+    BadRequest: 1,
+    NotFound: 2,
+    Error: 3
+}
 
-// Write your JavaScript code.
+$.ShowConfirm = function (contentMessage,okFunction) {
+    $.confirm({
+        title: 'Uyarı!',
+        content: contentMessage,
+        type: 'blue',
+        typeAnimated: true,
+        closeIcon: true,
+        buttons: {
+            Ok: {
+                text: 'Onaylıyorum',
+                btnClass: 'btn-red',
+                action: okFunction
+            },
+            close: function () {
+            }
+        }
+    });
+}
+
+$.ShowSuccess = function (contentMessage) {
+    $.alert({
+        title: 'Başarılı!',
+        content: contentMessage,
+        type: 'green',
+        typeAnimated: true,
+        closeIcon: true
+    });
+}
+
+
+$.ShowError = function (contentMessage) {
+    $.alert({
+        title: 'Hata!',
+        content: contentMessage,
+        type: 'red',
+        typeAnimated: true,
+        closeIcon: true
+    });
+}
+
+$.AjaxTableDelete = function (reqUrl, confirmMessage, deleteItemId, successFunc, errorFunc) {
+    $.ShowConfirm
+    (
+        confirmMessage,
+        function () {
+            $.ajax({
+                type: "POST",
+                url: reqUrl,
+                contentType: "application/json",
+                dataType: "json",
+                data: JSON.stringify({
+                    id: deleteItemId
+                }),
+                success: successFunc,
+                error: errorFunc
+            }); 
+        }        
+    )
+        
+}
